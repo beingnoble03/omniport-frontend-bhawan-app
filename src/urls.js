fb3f525f56949f855b93e3f28ba69fa5bb0be7b6
SuyashSalampuria <suyash546@gmail.com>
2020-04-14 00:53:30 +0530

    add urls.js

---
+++ src/urls.js
@@ -0,0 +1,35 @@
+import config from '../config.json'
+
+// Frontend URLs
+
+export const baseNavUrl = forwardLink => {
+  return `${config.baseUrl}${forwardLink}`
+}
+
+// Backend URLs
+
+export const baseApiUrl = () => {
+  return `/api/bhawan_app/`
+}
+
+export const complainsUrl = (residence) => {
+    return `${baseApiUrl()}${residence}/complaint/`
+  }
+
+export const statusComplainsUrl = (residence, statuses) => {
+    let allStatus = "?"
+    statuses.map( status => {
+      status = status.toUpperCase()
+      allStatus = `${allStatus}status=${status}&`
+    })
+    return `${baseApiUrl()}${residence}/complaint/${allStatus}&`
+}
+
+export const bookingsUrl = (residence) => {
+  return `${baseApiUrl()}${residence}/room_booking/`
+}
+
+export const statusBookingsUrl = (residence, status) => {
+  status = status.toUpperCase()
+  return `${baseApiUrl()}${residence}/room_booking/?status=${status}`
+}
