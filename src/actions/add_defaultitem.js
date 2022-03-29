f81615d1ff10e354421156946f3ba91f665e1509
vaishnavi-gupta18 <vaishnavi_g@me.iitr.ac.in>
2022-03-29 18:03:03 +0530

    Fix bugs

--- src/actions/add_defaultitem.js
+++ src/actions/add_defaultitem.js
@@ -23,6 +23,7 @@ export const addDefaultItem = (data, residence, successCallBack, errCallBack) =>
           icon: 'smile outline',
           time: 4000
         })
+        console.log(res)
         successCallBack(res)
         const response = {
           status: true,
@@ -34,7 +35,6 @@ export const addDefaultItem = (data, residence, successCallBack, errCallBack) =>
         })
       })
       .catch((err) => {
-        console.log(err)
         toast({
           type: 'error',
           title: 'Item could not be created',
